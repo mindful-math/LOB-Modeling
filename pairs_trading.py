446905dc11ef0b6ace04aa2b6435639b91fc510e
Mark Oussoren <49008466+Mark-Oussoren@users.noreply.github.com>
2020-10-04 13:11:33 -0400

    Update pairs_trading.py

--- pairs_trading.py
+++ pairs_trading.py
@@ -4,33 +4,7 @@ import math
 from statsmodels.tsa.stattools import coint
 import scipy.io as sio
 from pathlib import Path
-
-
-def pairs_stats(s1, s2):
-    """
-    :param s1: asset 1 as a numpy array
-    :param s2: asset 2 as a numpy array
-    :return: correlation, stationarity (just summary stats), cointegration (p-value)
-    """
-    corr = np.corrcoef(s1, s2)
-    cointegration = coint(s1, s2)
-    #
-    # just print summary stats for deciding stationarity
-    #
-    s1_0, s1_1, s1_2 = np.split(s1, 3)
-    s2_0, s2_1, s2_2 = np.split(s2, 3)
-    s1_0_mean, s1_1_mean, s1_2_mean = s1_0.mean(), s1_1.mean(), s1_2.mean()
-    s2_0_mean, s2_1_mean, s2_2_mean = s2_0.mean(), s2_1.mean(), s2_2.mean()
-    s1_0_var, s1_1_var, s1_2_var = s1_0.var(), s1_1.var(), s1_2.var()
-    s2_0_var, s2_1_var, s2_2_var = s2_0.var(), s2_1.var(), s2_2.var()
-
-    print("Correlation: " + str(corr) + "\n")
-    print("s1 Split Means: " + str(s1_0_mean) + ', ' + str(s1_1_mean) + ', ' + str(s1_2_mean) + "\n")
-    print("s1 Split Vars: " + str(s1_0_var) + ', ' + str(s1_1_var) + ', ' + str(s1_2_var) + "\n")
-    print("s2 Split Means: " + str(s2_0_mean) + ', ' + str(s2_1_mean) + ', ' + str(s2_2_mean) + "\n")
-    print("s2 Split Vars: " + str(s2_0_var) + ', ' + str(s2_1_var) + ', ' + str(s2_2_var) + "\n")
-    print("p Value for s1-x*s2: " + str(cointegration))
-
+import matplotlib.pyplot as plt
 
 def init_asset(stock):
     '''
@@ -50,8 +24,41 @@ def init_asset(stock):
     return {'time': t, 'bid': bid, 'bidvol': bidvol, 'ask': ask, 'askvol': askvol, 'market_order': market_order, 'dt': dt, 'midprice': midprice, 'microprice': microprice, 'spread': spread}
 
 
+def pairs_stats(s1, s2):
+    '''
+    :param s1: asset 1 as a numpy array
+    :param s2: asset 2 as a numpy array
+    :return: correlation, stationarity (just summary stats), cointegration (p-value)
+    '''
+    corr = np.corrcoef(s1, s2)
+    cointegration = coint(s1, s2)
+    #
+    # just print summary stats for deciding stationarity
+    #
+    s1_0, s1_1, s1_2 = np.split(s1, 3)
+    s2_0, s2_1, s2_2 = np.split(s2, 3)
+    s1_0_mean, s1_1_mean, s1_2_mean = s1_0.mean(), s1_1.mean(), s1_2.mean()
+    s2_0_mean, s2_1_mean, s2_2_mean = s2_0.mean(), s2_1.mean(), s2_2.mean()
+    s1_0_var, s1_1_var, s1_2_var = s1_0.var(), s1_1.var(), s1_2.var()
+    s2_0_var, s2_1_var, s2_2_var = s2_0.var(), s2_1.var(), s2_2.var()
+
+    print("Correlation: " + str(corr) + "\n")
+    print("s1 Split Means: " + str(s1_0_mean) + ', ' + str(s1_1_mean) + ', ' + str(s1_2_mean) + "\n")
+    print("s1 Split Vars: " + str(s1_0_var) + ', ' + str(s1_1_var) + ', ' + str(s1_2_var) + "\n")
+    print("s2 Split Means: " + str(s2_0_mean) + ', ' + str(s2_1_mean) + ', ' + str(s2_2_mean) + "\n")
+    print("s2 Split Vars: " + str(s2_0_var) + ', ' + str(s2_1_var) + ', ' + str(s2_2_var) + "\n")
+    print("p Value for s1-x*s2: " + str(cointegration))
+    
+    #
+    # check spread
+    #
+    dif = s1 - s2
+    dif.plot()
+    plt.axhline(dif.mean(), color = 'black')
+    
+
+# example (amzn isn't stationary, two are cointegrated if choose p=.015, spread ~ normal)
 amzn = init_asset(sio.loadmat(Path('Nov-2014/AMZN_20141103.mat').absolute())['LOB'])
 ebay = init_asset(sio.loadmat(Path('Nov-2014/EBAY_20141103.mat').absolute())['LOB'])
-
-
 pairs_stats(amzn['midprice'], ebay['midprice'])
+
